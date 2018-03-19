0e7909868d06af9a356b91d31b55b23e5a8e4980
Chinczykk <gamon.lama@gmail.com>
2018-03-19 23:15:21 +0100

    Change register form to forms generated by django

---
+++ forum/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.forms import ValidationError
+
+class RegisterForm(forms.ModelForm):
+    login = forms.CharField(max_length=20,
+                            min_length=5,
+                            error_messages={'min_length': 'This field should have atleast 5 characters'},
+                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(min_length=8,
+                            error_messages={'min_length': 'This field should have atleast 8  characters'},
+                            widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password = forms.CharField(error_messages={'min_length': 'This field should have atleast 5 characters'},
+                                    widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = User
+        fields = ('login', 'password', 'confirm_password', 'email')
+
+    def clean(self):
+        cleaned_data = super(RegisterForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                {"confirm_password": ["Passwords are not equal",]}
--- forum/views/accountViews.py
+++ forum/views/accountViews.py
@@ -1,56 +1,21 @@
 from django.shortcuts import render
 from .. import services
 from .. import services
+from ..forms import RegisterForm
 import re
 
 def register(request):
-    errors = {'login': '', 'password': '', 'confirmPassword': '', 'email': '', 'isError': False}
-    valid = {'login': '', 'password': '', 'confirmPassword': '', 'email': ''}
     if request.method == 'POST':
-        login = request.POST.get("login", "")
-        password = request.POST.get("password", "")
-        confirmPassword = request.POST.get("confirmPassword", "")
-        email = request.POST.get("email", "")
-        if not login:
-            errors['login'] = 'Please tell us your login'
-        elif len(login) < 5:
-            errors['login'] = 'Login should have atleast 5 characters'
-        elif services.check_if_login_exists(login):
-            errors['login'] = 'This login is already taken!'
-        if not password:
-            errors['password'] = 'Please tell us your password'
-        elif len(password) < 8:
-            errors['password'] = 'Password should have atleast 8 characters'
-        if not confirmPassword:
-            errors['confirmPassword'] = 'Please confirm your password'
-        elif confirmPassword != password:
-            errors['confirmPassword'] = 'Passwords are not equal'
-        if not email:
-            errors['email'] = 'Please tell us your email'
-        elif not re.search('(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', email):
-            errors['email'] = 'This is not correct email!'
-        elif services.check_if_email_exists(email):
-            errors['email'] = 'Another account is already registered to this email'
-
-        if errors['login'] != '' or errors['password'] != '' or errors['confirmPassword'] != '' or errors['email'] != '':
-            errors['isError'] = True
-
-            if errors['login'] == '':
-                valid['login'] = login
-            if errors['password'] == '':
-                valid['password'] = password
-            if errors['confirmPassword'] == '':
-                valid['confirmPassword'] = confirmPassword
-            if errors['email'] == '':
-                valid['email'] = email
-        
-        else:
-            services.add_user(login, password, email)
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            services.add_user(cd["login"], cd["password"], cd["email"])
             info = 'Your account is ready. You can log in now.'
             return render(request, 'forum/account/login.html', {'info': info})
+    else :
+        form = RegisterForm()
+    return render(request, 'forum/account/register.html', {'form': form})
 
-    return render(request, 'forum/account/register.html', {'errors': errors,
-                                                           'valid': valid})
 def login(request):
     errors = {'login': '', 'password': '', 'bad_login_or_password': '', 'isError': False}
     valid = {'login': ''}
